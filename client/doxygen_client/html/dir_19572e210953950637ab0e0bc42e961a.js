5f97caec30cb70bd76068fed0aa0d3a0798a1e9a
Dyamgrs <yamdiana.rk@mail.ru>
2025-06-26 12:52:04 +0300

    Delete client/doxygen_client directory

--- client/doxygen_client/html/dir_19572e210953950637ab0e0bc42e961a.js
+++
@@ -1,6 +0,0 @@
-var dir_19572e210953950637ab0e0bc42e961a =
-[
-    [ "client_autogen", "dir_d98c941cf4f3a2051f87db788d8bca4e.html", "dir_d98c941cf4f3a2051f87db788d8bca4e" ],
-    [ "test_caesar_autogen", "dir_e56f4b76a6981a9bd32eb8ead8ba4474.html", "dir_e56f4b76a6981a9bd32eb8ead8ba4474" ],
-    [ "test_hash_autogen", "dir_0eb28f51ffc0f5eea1d6d0472f04984d.html", "dir_0eb28f51ffc0f5eea1d6d0472f04984d" ]
-];
--- client/doxygen_client/html/dir_f226e7530d7413eed536923a3bc1464e.js
+++
@@ -1,7 +0,0 @@
-var dir_f226e7530d7413eed536923a3bc1464e =
-[
-    [ "ui_authorization.h", "ui__authorization_8h_source.html", null ],
-    [ "ui_mainwindow.h", "ui__mainwindow_8h_source.html", null ],
-    [ "ui_task1_caesar.h", "client__autogen_2include_2ui__task1__caesar_8h_source.html", null ],
-    [ "ui_task2_hash.h", "client__autogen_2include_2ui__task2__hash_8h_source.html", null ]
-];
