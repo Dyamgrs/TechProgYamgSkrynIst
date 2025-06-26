df09cbd48e6323c81ae20999a21cc69d5087b1f7
Dyamgrs <yamdiana.rk@mail.ru>
2025-06-26 13:00:57 +0300

    Delete doxygen_client directory

--- doxygen_client/html/dir_e56f4b76a6981a9bd32eb8ead8ba4474.js
+++
@@ -1,6 +0,0 @@
-var dir_e56f4b76a6981a9bd32eb8ead8ba4474 =
-[
-    [ "EWIEGA46WW", "dir_30507c8efe94ac809d642aa3770a1738.html", "dir_30507c8efe94ac809d642aa3770a1738" ],
-    [ "include", "dir_6c2e37d2e9fd69a99c1695685e013ccf.html", "dir_6c2e37d2e9fd69a99c1695685e013ccf" ],
-    [ "moc_predefs.h", "test__caesar__autogen_2moc__predefs_8h_source.html", null ]
-];
--- doxygen_client/html/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['qt_5fmeta_5ftag_5fzn10mainwindowe_5ft_0',['qt_meta_tag_ZN10MainWindowE_t',['../struct_q_t___w_a_r_n_i_n_g___d_i_s_a_b_l_e___d_e_p_r_e_c_a_t_e_d_1_1qt__meta__tag___z_n10_main_window_e__t.html',1,'QT_WARNING_DISABLE_DEPRECATED']]],
-  ['qt_5fmeta_5ftag_5fzn10task2_5fhashe_5ft_1',['qt_meta_tag_ZN10task2_hashE_t',['../struct_q_t___w_a_r_n_i_n_g___d_i_s_a_b_l_e___d_e_p_r_e_c_a_t_e_d_1_1qt__meta__tag___z_n10task2__hash_e__t.html',1,'QT_WARNING_DISABLE_DEPRECATED']]],
-  ['qt_5fmeta_5ftag_5fzn12task1_5fcaesare_5ft_2',['qt_meta_tag_ZN12Task1_CaesarE_t',['../struct_q_t___w_a_r_n_i_n_g___d_i_s_a_b_l_e___d_e_p_r_e_c_a_t_e_d_1_1qt__meta__tag___z_n12_task1___caesar_e__t.html',1,'QT_WARNING_DISABLE_DEPRECATED']]],
-  ['qt_5fmeta_5ftag_5fzn13authorizatione_5ft_3',['qt_meta_tag_ZN13authorizationE_t',['../struct_q_t___w_a_r_n_i_n_g___d_i_s_a_b_l_e___d_e_p_r_e_c_a_t_e_d_1_1qt__meta__tag___z_n13authorization_e__t.html',1,'QT_WARNING_DISABLE_DEPRECATED']]]
-];
