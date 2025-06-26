94b818dca46d04b0787a4111bd54330686f61f58
Dyamgrs <yamdiana.rk@mail.ru>
2025-06-26 13:03:36 +0300

    Delete docs directory

--- docs/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['singleton_5fclient_0',['Singleton_client',['../class_singleton__client.html',1,'']]],
-  ['singleton_5fclientdestroyer_1',['Singleton_clientDestroyer',['../class_singleton__client_destroyer.html',1,'']]]
-];
